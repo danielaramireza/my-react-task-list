fb4fc67c9d678fe94048f61bb575befd50171101
daniela ramirez alvarez <dani.maramirez@gmail.com>
2023-03-26 11:58:17 -0500

    Se creó la versión inicial del componente TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
     <div className="App">
       <Header titulo="Todo App" />
+      <TaskList />
     </div>
   );
 }
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,16 @@
+const TaskList = () => {
+  return (
+    <ul>
+      <li>
+        <div>
+          <input type="checkbox"></input>
+          <p>Tarea 1</p>
+          <button type="button">Editar</button>
+          <button type="button">Eliminar</button>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default TaskList;
