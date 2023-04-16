6f984390d708fa4b18ac98dbb0af544b25cac1db
daniela ramirez alvarez <dani.maramirez@gmail.com>
2023-04-15 20:43:32 -0500

    Se agrego la funcionalidad de crear nuevas tareas

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,23 @@ import Input from "./components/Input";
 function App() {
   const [listaDeTareas, setListaDeTareas] = useState([]);
 
+  function retornarUltimoId() {
+    const ultimoId = listaDeTareas.reduce((max, objeto) => {
+      console.log(objeto);
+      if (objeto.id > max) {
+        return objeto.id;
+      }
+      return max;
+    }, 0);
+    return ultimoId + 1;
+  }
+
   function agregarTarea(nombreTarea) {
     console.log(nombreTarea);
+    setListaDeTareas([
+      ...listaDeTareas,
+      { id: retornarUltimoId(), TaskName: nombreTarea, State: false },
+    ]);
   }
 
   return (
