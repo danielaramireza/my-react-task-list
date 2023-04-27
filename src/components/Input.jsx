98cfd8e8c341b7107fc674ca4b198f1f84571221
daniela ramirez alvarez <dani.maramirez@gmail.com>
2023-04-26 20:59:40 -0500

    Se implemento react-hook-form para el manejo de formularios y validaciones. Se actualizo el campo de nuevaTarea.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,27 +1,42 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Input(props) {
   const { agregarTarea } = props;
-  const [nombreTarea, setNombreTarea] = useState("");
 
-  function enviarTarea(event) {
-    event.preventDefault();
-    if (nombreTarea.length < 1) {
-      return;
-    }
-    agregarTarea(nombreTarea);
-    setNombreTarea("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    mode: "onChange",
+  });
+
+  function enviarTarea(dataFormulario) {
+    agregarTarea(dataFormulario.nombreTarea);
+    reset();
   }
   return (
-    <form className="formularioNuevaTarea" onSubmit={enviarTarea}>
-      <input
-        type="text"
-        placeholder="Agregar nueva tarea"
-        value={nombreTarea}
-        onChange={(ev) => setNombreTarea(ev.target.value)}
-      />
+    <form className="formularioNuevaTarea" onSubmit={handleSubmit(enviarTarea)}>
+      <div className="contenedorNuevaTarea">
+        <input
+          className="inputNuevaTarea"
+          type="text"
+          placeholder="Agregar nueva tarea"
+          {...register("nombreTarea", {
+            minLength: {
+              value: 3,
+              message: "El nombre de la tarea debe ser mayor a 3 caracteres",
+            },
+          })}
+        />
+        <span className="error" role="alert">
+          {errors.nombreTarea && errors.nombreTarea.message}
+        </span>
+      </div>
       <button type="submit">
         <FontAwesomeIcon icon={faPlus} />
       </button>
